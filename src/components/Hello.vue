1c866e320ddebe676fe124c749a81784d792a531
choijisoo <gsuchoi@neighbor21.co.kr>
2021-06-11 17:57:23 +0900

    Add: bootstrap-vue 패키지 설치 (align이 적용이 안됨)

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="hello">
-    <h3>{{ todo }}</h3>
-    <ul v-if="toDoItems && toDoItems.length">
-      <li v-for="toDoItem of toDoItems" v-bind:key="toDoItem">
-        {{toDoItem.title}}
-      </li>
-    </ul>
+    <!-- <h3>{{ todo }}</h3> -->
+    <b-card
+    header="오늘 해야 할 일"
+    style="max-width: 40rem; margin: auto; margin-top: 10vh;"
+    class="mb-2"
+    border-variant="info"
+    align="left">
+
+      <b-from-group id="to-do-input">
+        <b-container fluid>
+          <b-row class="my-1">
+            <b-col sm="10">
+              <b-form-input v-model="title" type="text" placeholder="새 할 일을 적으세요."/>
+            </b-col>
+            <b-col sm="2">
+              <b-button variant="outline-primary">추가</b-button>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-from-group>
+
+      <b-list-group v-if="toDoItems && toDoItems.length">
+        <b-list-group-item
+          v-for="toDoItem of toDoItems"
+          v-bind:data="toDoItem.title"
+          v-bind:key="toDoItem.id">
+          <b-form-checkbox
+            v-model="toDoItem.done">
+              {{toDoItem.title}}
+          </b-form-checkbox>
+        </b-list-group-item>
+      </b-list-group>
+      
+    </b-card>
   </div>
 </template>
 
 <script>
-import axios from 'axios' // 아깐 받은 axios 패키지를 사용하기 위해 import.
+import axios from 'axios'
 
 export default {
   name: 'hello',
